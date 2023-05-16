5123189546b24a72ad116fb45dbde6cf3dc2dc91
ほわいと <n-shiraki@mg.ngkntk.co.jp>
2023-05-16 13:44:51 +0900

    Add ws-libraries Plug-in

---
+++ wordpress-samples/wp-content/plugins/ws-libraries/ws-libraries.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: ライブラリ一括管理
+  Plugin URI: https://wtpage.info/
+  Description: phpのライブラリを一括管理
+  Author: ほわいとソフトウェア
+  Version: 0.0.1
+  Author URI: https://wtpage.info/
+*/
+
+
+?>
