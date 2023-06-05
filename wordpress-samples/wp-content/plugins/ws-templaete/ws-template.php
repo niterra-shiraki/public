f40c9c3e58255db13572c0e87255df2544d708a1
ほわいと <n-shiraki@mg.ngkntk.co.jp>
2023-06-05 09:34:42 +0900

    WordPressプラグインテンプレートを追加

---
+++ wordpress-samples/wp-content/plugins/ws-templaete/ws-template.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: ほわいとのプラグインテンプレート
+  Plugin URI: https://wtpage.info/
+  Description: プラグインのテンプレートファイル
+  Author: ほわいとソフトウェア
+  Version: 0.0.1
+  Author URI: https://wtpage.info/
+*/
+
+?>
