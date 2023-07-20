d8ff8df3a6df77036a9e4dde21cc3d41ec1a569e
ほわいと <n-shiraki@mg.ngkntk.co.jp>
2023-07-20 09:06:06 +0900

    色々更新

--- html-samples/README.md
+++ html-samples/README.md
@@ -1,32 +1,48 @@
 # html-samples
-HTMLサンプル集
+さまざまなHTMLやCSSのサンプル集．
+今のjavascriptはjQueryを要しないことが多いので，極力jQueryなしで書いている．
 
-## [css-samples](./css-samples/)
-CSSによる表現のサンプル
-
-## [chartjs](./chartjs/)
-Chart.jsによるグラフ描画サンプル
-
-## [google-chart](./google-chart/)
-Google Chartによるグラフ描画サンプル
+## [google-fonts](./google-fonts/)
+Google Fonts をCDN経由で読み込むサンプル
 
 ## [fade-box](./fade-box/)
 長い文章を短く表示して「もっと見る」ボタンで拡大表示．脱jQueryで実装．javascriptはclassで実装したが，まだ初心者なのでご愛嬌．
 
-## [google-fonts](./google-fonts/)
-Google Fonts を利用するサンプル
+## [tab-box](./tab-box/)
+タブ切り替え要素．脱jQueryで実装．
 
-## [read-mode](./read-more/)
+## [read-more](./read-more/)
 「もっと見る」のサンプル
 
-## [swiper](./swiper/)
-swiper.jsによるカルーセル（スライダー）のサンプル
+## [google-chart](./google-chart/)
+Google Chart を使ってグラフを表示
 
-## [tab-box](./tab-box/)
-タブ切り替えのサンプル
+## [chartjs](./chartjs/)
+Chart.js を使ってグラフを表示
+
+## [calc-physics](./calc-physics/)
+等加速度運動を計算
+
+## [css-samples](./css-samples/)
+CSSによるデザイン表現のサンプル
+
+## [position](./position/)
+いろいろな位置調整方法
+
+## [check-radio](./check-radio/)
+チェックボックスやラジオボタンのサンプル
 
 ## [wait-modal](./wait-modal/)
-待ちアニメーションのサンプル
+「待ちアニメーション」のサンプル
 
-## [upload-image](./upload-image/)
-画像をアップロードしてすぐプレビュー
+## [slick](./slick/)
+slickライブラリによるカルーセル（スライダー）
+
+## [swiper](./swiper/)
+swiperライブラリによるカルーセル（スライダー）
+
+## [sensor](./sensor/)
+javascriptでGPSと加速度計測
+
+## [canvas-draw](./canvas-draw/)
+canvasにお絵描きして画像ファイルとしてダウンロード
---
+++ html-samples/sensor/js/acc.js
@@ -0,0 +1,75 @@
+//////////////////////////////////////////////////////////////////////
+//	acc.js
+//		Last Change: Thu 13 July 2023  17:50:56
+//////////////////////////////////////////////////////////////////////
+
+
+var _acc_elem1_ = false;
+var _acc_elem2_ = false;
+
+function startACC(id1, id2) {
+    _acc_elem1_ = document.getElementById(id1);
+    _acc_elem2_ = document.getElementById(id2);
+
+    if ( window.DeviceMotionEvent && window.DeviceMotionEvent.requestPermission ) {
+        DeviceMotionEvent.requestPermission()
+            .then( (state) => {
+                if ( state === 'granted' ) {
+                    window.addEventListener('devicemotion', accDeviceMotion, true);
+                }
+            })
+            .catch( (err) => console.error(err) );
+    }
+    else {
+        _acc_elem1_.textContent = '（加速度センサなし）';
+    }
+
+    if ( window.DeviceOrientationEvent && window.DeviceOrientationEvent.requestPermission ) {
+        DeviceOrientationEvent.requestPermission()
+            .then( (state) => {
+                if ( state === 'granted' ) {
+                    window.addEventListener('deviceorientation', accDeviceOrientation, true);
+                }
+            })
+            .catch( (err) => console.error(err) );
+    }
+    else {
+        _acc_elem2_.textContent = '（Gセンサなし）';
+    }
+
+    return;
+}
+
+function accDeviceMotion(event) {
+    var x = event.accelerationIncludingGravity.x;
+    var y = event.accelerationIncludingGravity.y;
+    var z = event.accelerationIncludingGravity.z;
+            
+    _acc_elem1_.innerHTML = 'hoge';
+
+    if ( _acc_elem1_ ) {
+	_acc_elem1_.innerHTML = 
+	    '<div> X：'+ Math.round(x * 10) / 10 +' </div>' +
+	    '<div> Y：'+ Math.round(y * 10) / 10 +' </div>' + 
+	    '<div> Z：'+ Math.round(z * 10) / 10 +' </div>';
+    }
+
+    return;
+}
+
+function accDeviceOrientation(event) {
+    var alpha = event.alpha;
+    var beta  = event.beta;
+    var gamma = event.gamma;
+            
+    _acc_elem2_.innerHTML = 'fuga';
+
+    if ( _acc_elem2_ ) {
+	_acc_elem2_.innerHTML = 
+	    '<div> alpha：'+ Math.round(alpha * 10) / 10 +' </div>' +
+	    '<div> beta ：'+ Math.round(beta  * 10) / 10 +' </div>' + 
+	    '<div> gamma：'+ Math.round(gamma * 10) / 10 +' </div>';
+    }
+
+    return;
+}
